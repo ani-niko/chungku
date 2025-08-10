e7688dd1f57b7b4d6d5d67562c8a7d88e40de30e
ani-niko <ani.nikolaevna.2000@gmail.com>
2025-08-10 17:52:13 +0545

    Update

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ yes.addEventListener("click", () => {
   vid.style.display = "block";
   gif.src = "../resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Knew it Aum 😘";
-  //yes.innerHTML = '<a href="https://www.instagram.com/loop.codes/">Message me</a>';// inside the " " put your social media profile link
+  yes.innerHTML = "I love you too 😘💖";
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
@@ -66,4 +66,5 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
+
 
