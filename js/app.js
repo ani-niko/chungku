201044f37851d890e7481f1b4397461dd9252dae
ani-niko <ani.nikolaevna.2000@gmail.com>
2025-08-10 17:57:47 +0545

    Update

--- js/app.js
+++ js/app.js
@@ -56,7 +56,7 @@ no.addEventListener("click", () => {
 // changes for when YES button is clicked
 yes.addEventListener("click", () => {
   vid.style.display = "block";
-  gif.src = "../resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
+  gif.src = "resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Knew it Aum 😘";
   yes.innerHTML = "I love you too 😘💖";
   yes.style.height = "90%";
@@ -66,6 +66,7 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
+
 
 
 
